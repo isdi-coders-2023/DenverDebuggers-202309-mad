1e8036f78cfa36093822cc0ec902446cd4017802
Andrea Lopez <andrealoga33@gmail.com>
2023-11-06 13:04:03 +0100

    Add models to the proyect

--- src/models/character.ts
+++ src/models/character.ts
@@ -1,1 +1,13 @@
 
+type Estado = 'Vivo' | 'Fallecido' | 'Ficticio';
+
+
+export type Character = {
+  _id: string;
+  Nombre: string;
+  Historia: string;
+  Imagen: string;
+  Genero: string;
+  Estado: Estado;
+  Ocupacion: string;
+};
