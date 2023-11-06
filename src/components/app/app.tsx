7a1bcf8cf8221ac41427c484b5df25ab119d4eb4
Andrea Lopez <andrealoga33@gmail.com>
2023-11-06 16:55:01 +0100

    Fix Audit problems

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,6 +3,6 @@ import { Footer } from "../footer/footer";
 
 export function App() {
   return <>
- <Footer></Footer> 
+    <Footer></Footer> 
   </>;
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,14 +1,18 @@
-import './footer.scss'
+import './footer.scss';
 
-export function Footer () {
-
+export function Footer() {
   return (
     <footer>
-      <div className = "footer-container">
-        <p className = 'footer-text'> Powered by: The Simpsons API </p>
-        <img className='footer-image' src='footer.png' alt="The Simpsons API footer" width="82" height="80"/>
+      <div className="footer-container">
+        <p className="footer-text"> Powered by: The Simpsons API </p>
+        <img
+          className="footer-image"
+          src="footer.png"
+          alt="The Simpsons API footer"
+          width="82"
+          height="80"
+        />
       </div>
     </footer>
   );
 }
-
