1fd79450905680d6a8c7580336c8c0e74b3b0eab
Andrea Lopez <andrealoga33@gmail.com>
2023-11-07 15:39:05 +0100

    Fix sonarcloud error

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,4 @@
 import { List } from '../list/list';
-import './app.scss';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
 import { Pagination } from '../pagination/pagination';
