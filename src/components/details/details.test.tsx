86a4b961ee7f2807596ba74ee8be7ad99f539808
Andrea Lopez <andrealoga33@gmail.com>
2023-11-08 18:53:56 +0100

    Add details test

---
+++ src/components/details/details.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import { AppContext, ContextStructure } from '../../context/app.context';
+import { Details } from './details';
+import '@testing-library/jest-dom';
+import { Character } from '../../models/character';
+import { State } from '../../reducer/actions';
+import { BrowserRouter } from 'react-router-dom';
+const mockContext: ContextStructure = {
+  state: {
+    characters: [{ Nombre: 'Bart' }],
+    page: 1,
+  } as State,
+  loadCharacters: jest
+    .fn()
+    .mockResolvedValue([{ Nombre: 'Bart' } as Character]),
+} as unknown as ContextStructure;
+
+describe('Given List component', () => {
+  describe('When we instantiate', () => {
+    beforeEach(() => {
+      render(
+        <BrowserRouter>
+          <AppContext.Provider value={mockContext}>
+            <Details></Details>
+          </AppContext.Provider>
+        </BrowserRouter>
+      );
+    });
+    test('renders List with Card', () => {
+      const element = screen.getByRole('heading');
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -9,28 +9,26 @@ export function Details() {
   const character = characters.find((element) => element._id === id);
 
   return (
-    <>
-      <div className="details-container">
-        <div className="image-container">
-          <img src={character?.Imagen} alt={character?.Nombre} />
-        </div>
-        <div className="info-container">
-          <h2>{character?.Nombre}</h2>
-          <p>{character?.Historia}</p>
-          <p>
-            <span>Estado: </span>
-            {character?.Estado}
-          </p>
-          <p>
-            <span>Género: </span>
-            {character?.Genero}
-          </p>
-          <p>
-            <span>Ocupación: </span>
-            {character?.Ocupacion}
-          </p>
-        </div>
+    <div className="details-container">
+      <div className="image-container">
+        <img src={character?.Imagen} alt={character?.Nombre} />
       </div>
-    </>
+      <div className="info-container">
+        <h2>{character?.Nombre}</h2>
+        <p>{character?.Historia}</p>
+        <p>
+          <span>Estado: </span>
+          {character?.Estado}
+        </p>
+        <p>
+          <span>Género: </span>
+          {character?.Genero}
+        </p>
+        <p>
+          <span>Ocupación: </span>
+          {character?.Ocupacion}
+        </p>
+      </div>
+    </div>
   );
 }
