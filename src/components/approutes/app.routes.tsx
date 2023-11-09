8547f3ea28e9ab67e120dfd7c88a036a6414cff9
Andrea Lopez <andrealoga33@gmail.com>
2023-11-09 10:57:43 +0100

    Fix some bugs and fix test

--- src/components/approutes/app.routes.tsx
+++ src/components/approutes/app.routes.tsx
@@ -1,10 +1,9 @@
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import ErrorPage from '../../pages/error/error';
 
 const Home = lazy(() => import('../../pages/home/home'));
 const Details = lazy(() => import('../../pages/details/details'));
-const Error = lazy(() => import('../../pages/error/error'));
+const ErrorPage = lazy(() => import('../../pages/error/error'));
 export function AppRoutes() {
   return (
     <main>
