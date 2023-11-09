543a975a760a0c5bb94e00dc49b17d3d55e35382
eliasmr98 <eliasmartinrivas@gmail.com>
2023-11-09 09:52:01 +0100

    Add handleFilter hook

--- src/hooks/use.character.tsx
+++ src/hooks/use.character.tsx
@@ -2,11 +2,14 @@ import { SyntheticEvent, useCallback, useMemo, useReducer } from 'react';
 import { ApiSimpsons } from '../services/api.repo';
 import { characterReducer } from '../reducer/reducer';
 import { loadActionCreator, changePage, State } from '../reducer/actions';
+import { useState } from 'react';
 
 export function useCharacters() {
   const initialValue: State = { characters: [], page: 1 };
   const [state, dispatch] = useReducer(characterReducer, initialValue);
-
+  const [filteredCharacters, setFilteredCharacters] = useState(
+    state.characters
+  );
   const repo = useMemo(() => new ApiSimpsons(state.page), [state.page]);
 
   const loadCharacters = useCallback(async () => {
@@ -30,10 +33,22 @@ export function useCharacters() {
     dispatch(changePage(state.page - 1));
   };
 
+  const handleFilter = (event: SyntheticEvent) => {
+    event.preventDefault();
+    const element = event.target as HTMLInputElement;
+    const value = element.value;
+    const filtered = state.characters.filter((character) =>
+      character.Estado.includes(value)
+    );
+    setFilteredCharacters(filtered);
+  };
+
   return {
     loadCharacters,
     handleNext,
     handlePrevious,
     state,
+    filteredCharacters,
+    handleFilter,
   };
 }
