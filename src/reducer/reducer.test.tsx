89e598eb035f618eb6e7f346b3e785a972d04a8e
Luis Puig <lpuig89@gmail.com>
2023-11-11 17:09:13 +0100

    Fix duplicate

--- src/reducer/reducer.test.tsx
+++ src/reducer/reducer.test.tsx
@@ -2,187 +2,95 @@ import { characterReducer } from './reducer';
 import { Character } from '../models/character';
 
 describe('Auth Reducer', () => {
+  const mockPayload = {
+    characters: [],
+    page: 1,
+    filteredCharacters: [],
+    selectedValue: '',
+  };
+  const mockPayload2 = {
+    characters: [{} as Character],
+    page: 1,
+    filteredCharacters: [],
+    selectedValue: '',
+  };
   test('should return load correclty ', () => {
-    const mockPayload = {
-      characters: [],
-      page: 1,
-      filteredCharacters: [],
-      selectedValue: '',
-    };
+    mockPayload;
 
-    const newState = characterReducer(
-      {
-        characters: [],
-        page: 1,
-        filteredCharacters: [],
-        selectedValue: '',
-      },
-      {
-        type: 'load',
-        payload: [],
-      }
-    );
+    const newState = characterReducer(mockPayload, {
+      type: 'load',
+      payload: [],
+    });
     expect(newState).toEqual(mockPayload);
   });
 
   test('should return load correclty ', () => {
-    const mockPayload = {
-      characters: [],
-      page: 1,
-      filteredCharacters: [],
-      selectedValue: '',
-    };
+    mockPayload;
 
-    const newState = characterReducer(
-      {
-        characters: [],
-        page: 1,
-        filteredCharacters: [],
-        selectedValue: '',
-      },
-      {
-        type: 'filter',
-        payload: [],
-      }
-    );
+    const newState = characterReducer(mockPayload, {
+      type: 'filter',
+      payload: [],
+    });
     expect(newState).toEqual(mockPayload);
   });
 
   test('should return page correclty ', () => {
-    const mockPayload = {
-      characters: [],
-      page: 1,
-      filteredCharacters: [],
-      selectedValue: '',
-    };
+    mockPayload;
 
-    const newState = characterReducer(
-      {
-        characters: [],
-        page: 1,
-        filteredCharacters: [],
-        selectedValue: '',
-      },
-      {
-        type: 'page',
-        payload: 1,
-      }
-    );
+    const newState = characterReducer(mockPayload, {
+      type: 'page',
+      payload: 1,
+    });
     expect(newState).toEqual(mockPayload);
   });
 
   test('should return Add correclty ', () => {
-    const mockPayload = {
-      characters: [{} as Character],
-      page: 1,
-      filteredCharacters: [],
-      selectedValue: '',
-    };
+    mockPayload2;
 
-    const newState = characterReducer(
-      {
-        characters: [],
-        page: 1,
-        filteredCharacters: [],
-        selectedValue: '',
-      },
-      {
-        type: 'create',
-        payload: {} as Character,
-      }
-    );
-    expect(newState).toEqual(mockPayload);
+    const newState = characterReducer(mockPayload, {
+      type: 'create',
+      payload: {} as Character,
+    });
+    expect(newState).toEqual(mockPayload2);
   });
 
   test('should return update correclty ', () => {
-    const mockPayload = {
-      characters: [{} as Character],
-      page: 1,
-      filteredCharacters: [],
-      selectedValue: '',
-    };
+    mockPayload2;
 
-    const newState = characterReducer(
-      {
-        characters: [{} as Character],
-        page: 1,
-        filteredCharacters: [],
-        selectedValue: '',
-      },
-      {
-        type: 'update',
-        payload: {} as Character,
-      }
-    );
-    expect(newState).toEqual(mockPayload);
+    const newState = characterReducer(mockPayload2, {
+      type: 'update',
+      payload: {} as Character,
+    });
+    expect(newState).toEqual(mockPayload2);
   });
 
   test('should return delete correclty ', () => {
-    const mockPayload = {
-      characters: [],
-      page: 1,
-      filteredCharacters: [],
-      selectedValue: '',
-    };
+    mockPayload;
 
-    const newState = characterReducer(
-      {
-        characters: [],
-        page: 1,
-        filteredCharacters: [],
-        selectedValue: '',
-      },
-      {
-        type: 'delete',
-        payload: 'string',
-      }
-    );
+    const newState = characterReducer(mockPayload, {
+      type: 'delete',
+      payload: 'string',
+    });
     expect(newState).toEqual(mockPayload);
   });
 
   test('should return selectValue correclty ', () => {
-    const mockPayload = {
-      characters: [],
-      page: 1,
-      filteredCharacters: [],
-      selectedValue: '',
-    };
+    mockPayload;
 
-    const newState = characterReducer(
-      {
-        characters: [],
-        page: 1,
-        filteredCharacters: [],
-        selectedValue: '',
-      },
-      {
-        type: 'select',
-        payload: '',
-      }
-    );
+    const newState = characterReducer(mockPayload, {
+      type: 'select',
+      payload: '',
+    });
     expect(newState).toEqual(mockPayload);
   });
 
   test('should return default correclty ', () => {
-    const mockPayload = {
-      characters: [],
-      page: 1,
-      filteredCharacters: [],
-      selectedValue: '',
-    };
+    mockPayload;
 
-    const newState = characterReducer(
-      {
-        characters: [],
-        page: 1,
-        filteredCharacters: [],
-        selectedValue: '',
-      },
-      {
-        type: '',
-        payload: null,
-      }
-    );
+    const newState = characterReducer(mockPayload, {
+      type: '',
+      payload: null,
+    });
     expect(newState).toEqual(mockPayload);
   });
 });
