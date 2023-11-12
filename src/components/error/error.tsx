40ee3b2a63e18738d7d3d21f65380e693d9d6b98
eliasmr98 <eliasmartinrivas@gmail.com>
2023-11-12 16:44:33 +0100

    Create new styles

--- src/components/error/error.tsx
+++ src/components/error/error.tsx
@@ -1,3 +1,19 @@
+import './error.scss';
 export function ErrorComponent() {
-  return <p>Error Message</p>;
+  return (
+    <div className="error-container">
+      <div className="error-img-container">
+        <img
+          src="./error-img.png"
+          alt="Homer doing doh expression"
+          height="260"
+          width="260"
+        />
+      </div>
+      <div className="error-text-container">
+        <p>DO-OH ERROR 404</p>
+        <p>La página no ha sido encontrada</p>
+      </div>
+    </div>
+  );
 }
